139b3ea4a58c8731433bb31818ab4e330d4a0976
docear <rdocea95@gmail.com>
2019-10-05 18:00:33 +0100

    Updating Extract.py script with variable paths

--- Extract.py
+++ Extract.py
@@ -19,6 +19,8 @@ window_width = 80
 
 paths = Path('/media/docear/My Passport/Kaggle/Hemorrhage/stage_1_train_images') # set the path to where your ~/stage_1_train_images folder is
 pathsTest = Path('/media/docear/My Passport/Kaggle/Hemorrhage/stage_1_test_images') # set the path to where your ~/stage_1_test_images folder is
+trainCSVpath = '/media/docear/My Passport/Kaggle/Hemorrhage/stage_1_train.csv'
+revisedtrainCSVpath = '/media/docear/My Passport/Kaggle/Hemorrhage/stage_1_train_revised.csv'
 
 path = list(paths.iterdir())[0]
 im = pydicom.read_file(str(path))
@@ -37,6 +39,13 @@ def resize(src, dst, sz):
     im = PIL.Image.fromarray(ary.astype(np.int8), mode='L')
     im.resize((sz,sz), resample=PIL.Image.BICUBIC).save(f'{dst}/{src.stem}.png')
     
+import pandas as pd
+df = pd.read_csv('/media/docear/My Passport/Kaggle/Hemorrhage/stage_1_train.csv') # path to your CSV
+df[df.ID.str.match('ID_6431af929')]
+df = df[~df.ID.str.match('ID_6431af929')]
+df.to_csv(revisedtrainCSVpath, index=False)
+
+    
 print('Processing Train Set')
 def resize_112(path, _): resize(path, '/media/docear/My Passport/Kaggle/Hemorrhage/data/112/train', 112) # set the destination file address to a new folder that will accept the processed training set
 parallel(resize_112, list(paths.iterdir()), max_workers=12)
@@ -44,3 +53,4 @@ parallel(resize_112, list(paths.iterdir()), max_workers=12)
 print('Processing Test Set')
 def resize_112_test(path, _): resize(path, '/media/docear/My Passport/Kaggle/Hemorrhage/data/112/test', 112) # set the destination file address to a new folder that will accept the processed test set
 parallel(resize_112_test, list(pathsTest.iterdir()), max_workers=12)
+
